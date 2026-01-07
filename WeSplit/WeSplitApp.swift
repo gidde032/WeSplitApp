2744b1b37a7ce3fd1f8c25c2e5697e46b0388f9b
Finn Gidden <gidde032@umn.edu>
2026-01-07 14:36:40 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Finn Gidden on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
